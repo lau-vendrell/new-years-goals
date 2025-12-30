d8b06d5855fa1ab898770c36da10a2854ffbe44e
Laura Vendrell <ve.aparisi@gmail.com>
2025-12-30 12:40:00 +0100

    Set Vite base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173
-  }
+  base: 'https://github.com/lau-vendrell/new-years-goals/',
 });
